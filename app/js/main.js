5f97177b3907bf000245c088eeda06d0f0886267
heindesign <hein@yandex.com>
2018-12-03 17:07:53 +0500

    creativity & portfolio done

---
+++ app/js/main.js
@@ -0,0 +1,24 @@
+/* isotope */
+
+$(window).on("load", function() {
+	$(".sort-by__item").on("click", function() {
+		$(".sort-by__item").removeClass("active");
+		$(this).addClass("active");
+
+		var data = $(this).attr("data-filter");
+		$workGrid.isotope({
+			filter: data
+		});
+	});
+	if (document.getElementById("work")) {
+		var $workGrid = $(".projects").isotope({
+			itemSelector: ".grid-item",
+			percentPosition: true,
+			masonry: {
+				columnWidth: ".grid-sizer"
+			}
+		});
+	}
+});
+
+/**/
