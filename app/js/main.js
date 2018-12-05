b474e3376ffc1dadf9abadbff15c05a7170238fa
heindesign <hein@yandex.com>
2018-12-05 16:02:15 +0500

    clients & about done

--- app/js/main.js
+++ app/js/main.js
@@ -22,3 +22,40 @@ $(window).on("load", function() {
 });
 
 /**/
+
+
+/* clients slider */
+
+function clientsSlider(){
+	if ( $('.clients-slider').length ){
+		$('.clients-slider').owlCarousel({
+			items: 5,
+			autoplay: true,
+			autoplayTimeout: 2500,
+			loop: true,
+			nav: false,
+			dots: false,
+			responsive: {
+				0: {
+					items: 1,
+				},
+				400: {
+					items: 2,
+				},
+				576: {
+					items: 3,
+				},
+				768: {
+					items: 4,
+				},
+				992: {
+					items: 5,
+				}
+				}
+			})
+		}
+	}
+
+clientsSlider();
+
+/**/
